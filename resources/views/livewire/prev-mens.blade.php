4a2f91674b9c7f56fe2cd5c74c29f4812537ed27
japhetBazungula <jeffbaz3004@gmail.com>
2024-01-28 15:11:46 +0100

    fin prevision mensuelle

--- resources/views/livewire/prev-mens.blade.php
+++ resources/views/livewire/prev-mens.blade.php
@@ -1,4 +1,40 @@
+@vite('resources/css/app.css') 
 <div>
+    <?php 
+        use App\Models\Loyer;
+        use App\Models\Locataire;
+    ?>
+    @php
+        $Mois1 = [
+            '01' => 'Janvier',
+            '02' => 'Février',
+            '03' => 'Mars',
+            '04' => 'Avril',
+            '05' => 'Mais',
+            '06' => 'Juin',
+            '07' => 'Juillet',
+            '08' => 'Aout',
+            '09' => 'Septembre',
+            '10' => 'Octobre',
+            '11' => 'Novembre',
+            '12' => 'Décembre'
+        ];
+        $Mois2 = [
+            'Janvier' => '01',
+            'Février' => '02',
+            'Mars' => '03',
+            'Avril' => '04',
+            'Mais' => '05',
+            'Juin' => '06',
+            'Juillet' => '07',
+            'Aout' => '08',
+            'Septembre' => '09',
+            'Octobre' => '10',
+            'Novembre' => '11',
+            'Décembre' => '12'
+        ];
+        
+    @endphp
     <div class="overflow-x-auto shadow-md sm:rounded-lg">
         <div class="inline-block min-w-full align-middle">
             <div class="overflow-hidden ">
@@ -28,33 +64,103 @@
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                         @php
+                            $_id = 0;
+                            $ctrR = 0;
+                            
                             $sommeGarentie = 0;
                             $sommeLoyerApay = 0;
                             $sommeLoyerPay = 0;
                             $voir = 0;
-                        @endphp
-                        @foreach ($data as $dt)
-                        @php
-                        /* ...........nombre d'occupations dans une galerie..................... */
-                        
-                        /* ...........nombre de locataires dans une occupation.................. */
+                            
+                            // $m est le mois parcouru enregistré pour le calcul de somme 
+                           $total = 0;
+                           $m = 0; // mois encour de traitement
+                           $total_mois = 0;
+                           $somme_mois = [];
+                           $nbrMois_paye = 0;
 
-                            for($i = 0; $i < $dt->occupations->count(); $i++){
-                                $sommeLoyerApay +=$dt->occupations[$i]->locataires->count() * $dt->occupations[$i]->montant;
-                                foreach ($dt->occupations[$i]->locataires as $loc) {
-                                    if($loc->actif){
-                                        
+                           /* total loyer */
+                           $totLoyer = 0;
+                           $totDette =  0;
+                        @endphp
+
+                        @foreach ($data as $dt)
+                        
+                        @php
+                            /* ...........nombre d'occupations dans une galerie..................... */
+                            
+                            /* ...........nombre de locataires dans une occupation.................. */
+                            
+                            foreach($dt->occupations as $occup){
+                                    $sommeLoyerApay += Locataire::where('occupation_id',$occup->id)->where('actif',true)->count() * $occup->montant;
+                                    /* $sommeLoyerApay +=$occup->locataires->count() * $occup->montant; */
+                                    foreach ($occup->locataires as $loc) {
+                                        if($loc->actif){
+                                                /* Parcourir les loyers du locataire */ 
+                                                foreach (Loyer::where('locataire_id', $loc->id)->orderByRaw('annee, mois')->get() as $loy)
+                                                {
+                                                        //convertir mois en nombre
+                                                        $mloyer = intval($Mois2[$loy->mois]);
+                                                        //si ce n'est pas le meme mois qu'on traite
+                                                        if($m != $mloyer){
+                                                            if($m != 0){
+                                                                //s'il a une dette par rapport a ce mois
+                                                                if ($total_mois < $loc->occupation->montant) {
+                                                                    /* @endphp
+                                                                    <p>{{$loc->loyers[$loop->index-1]->mois}} : {{$total_mois}} / {{$loc->occupation->montant}}</p>
+                                                                    @php */
+                                                                    $total += $loc->occupation->montant - $total_mois;
+                                                                }
+                                                            }
+                                                            //chargement du mois suivant et calcul de la somme des loyers payess
+                                                            $m = $mloyer;
+                                                            $total_mois = 0;
+                                                            $total_mois += $loy->montant;
+                                                            $nbrMois_paye++;
+                                                        }
+                                                        else{
+                                                            $total_mois += $loy->montant;
+                                                        }
+                                                }
+                                                /* Affichage des arrieres s'il y a */
+                                                    $Nba = date("Y") - $loc->ap; //nombre d'annee
+                                                    $mois_encours = date("m"); //mois encours
+                                                    $nbMois = ((13 * $Nba) - $loc->mp) + date("m"); //nombre de mois total
+                                                    $x_encour = ($Nba == 0) ? $mois_encours :  (13 - $loc->mp - $nbrMois_paye); // nombre de mois de l'annee precedente s'il y a 
+                                                
+                                                
+
+                                                /* Affichage de mois d'arrieressss */
+                                                if ($loc->ap != null)
+                                                {                                                       
+                                                        if ($x_encour >= 0){
+                                                            if ($x_encour > 0){    
+                                                                if ($Nba != 0){
+                                                                    for ($i = ($loc->mp + $nbrMois_paye); $i <= 12; $i++){
+                                                                            $total += $loc->occupation->montant;
+                                                                    }
+                                                                }else{
+                                                                    /* Si tout se passe dans la meme annee */
+                                                                    for ($i = ($loc->mp + $nbrMois_paye); $i <= $x_encour; $i++){
+                                                                            $total += $loc->occupation->montant;
+                                                                    }
+                                                                }
+                                                            }
+                                                            if ($Nba > 0){   
+                                                                for ($i = 1; $i <= $mois_encours; $i++){
+                                                                    $total += $loc->occupation->montant;
+                                                                }
+                                                            }
+                                                        }
+                                                }
+
+                                                
+                                                
+                                        }
+                                           
                                     }
                                 }
-                                /* for ($j=0; $j < $dt->occupations[$i]->locataires->count() ; $j++) { 
-                                    if ($dt->occupations[$i]->locataires[$j]->actif == true) {
-                                        for($k = 0; $k < $dt->occupations[$i]->locataires[$j]->loyers->count(); $k++){
-                                             dd($dt->occupations[$i]->locataires[$j]->loyers[$k]->montant;
-                                        } 
-                                    }
-                                } */
-
-                            }
+                        
                         @endphp
 
                         <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -79,16 +185,47 @@
                                  {{$sommeLoyerApay}}
                             </td>
                             <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{$total}}
+                            </td>
+                        </tr>
+                        @php
+                            $totLoyer += $sommeLoyerApay;
+                            $sommeLoyerApay = 0;
+                            $totDette += $total;
+                            $total = 0;
+                        //    $m = 0; // mois encour de traitement
+                        //    $total_mois = 0;
+                        //    $somme_mois = [];
+                        //    $nbrMois_paye = 0;
+                        @endphp
+                        @endforeach
+                        <tr class="bg-gray-600 dark:hover:bg-gray-700 text-white">
+                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 
+                            </td>
+                            <td class="py-4 px-6 text-sm font-medium  whitespace-nowrap dark:text-white">
+                                Total
                             </td>
                             <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 
                             </td>
+                            <td class="py-4 px-6 text-sm font-medium whitespace-nowrap dark:text-white">
+                                {{ $totLoyer }}
+                            </td>
+                            <td class="py-4 px-6 text-sm font-medium whitespace-nowrap dark:text-white">
+                                {{$totDette}}
+                            </td>
                         </tr>
-                        @php
-                            $sommeLoyerApay = 0;
-                        @endphp
-                        @endforeach
+                        <tr class="bg-gray-600 text-white">
+                            <td class="py-4 px-6 text-sm font-medium  whitespace-nowrap dark:text-white">
+                            </td>
+                            <td colspan="2" class="py-4 px-6 text-sm font-medium  whitespace-nowrap dark:text-white">
+                                Total prévision mensuelless 
+                            </td>
+                            <td colspan="2" class="py-4 px-6 text-sm font-medium  whitespace-nowrap dark:text-white">
+                                {{$totLoyer}} + {{$totDette}} = {{$totLoyer + $totDette }}
+                            </td>
+                        </tr>
                        
                     </tbody>
                 </table>
