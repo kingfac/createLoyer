3dcf65fa7dd4e8ac2479aa6163980bc9c57d1253
kingfac <glodin.kiese@gmail.com>
2024-02-21 14:31:22 +0100

    mois reg

--- app/Filament/Resources/LocataireResource.php
+++ app/Filament/Resources/LocataireResource.php
@@ -159,6 +159,24 @@ class LocataireResource extends Resource
                     ->dateTime()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('mp')
+                    ->default(function(Model $record){
+                        $ConvMois = [
+                            "1"=> "janvier",
+                            "2"=>"février", 
+                            "3"=>"mars",
+                            "4"=>"avril",
+                            "5"=>"mai",
+                            "6"=>"juin",
+                            "7"=>"juillet",
+                            "8"=>"aout",
+                            "9"=>"septembre",
+                            "10"=>"octobre",
+                            "11" => "novembre", 
+                            "12" => "décembre"
+                        ];
+
+                        return $ConvMois[$record->mp];
+                    })
                     ->sortable(),
                 
             ])
