1864f716bd3a7cb7ee3cb37a7abcea84505700fc
kingfac <glodin.kiese@gmail.com>
2024-03-23 19:35:49 +0100

    recu4

--- resources/views/anticipatif.blade.php
+++ resources/views/anticipatif.blade.php
@@ -1,5 +1,126 @@
 <link rel="stylesheet" href="{{public_path('css.css')}}">
 
+
+@php
+$lelo = new DateTime('now');
+$lelo = $lelo->format('d-m-Y');
+@endphp
+<table style=" width:100%; font-size: 1em; font-weight: bold; color:rgb(46, 131, 211)">
+    <tr  style="">
+        <td  style="">
+
+            <div class=" text-center" style="">
+                {{-- <img src="{{public_path('logo.png')}}"> --}}
+                <h2>MILLE ET UNE MERVEILLE</h2>
+                <h3>N.R.C. 53666 - Id. Nat. : 01-910-N 40270 K</h3>
+                <h3>Av. Tshuapa N°90 C./Kinshasa</h3>
+                <h3 style=" border-bottom:solid 1px; borcer-bottom-width:100px;">Tel. : 0850758588 - 0816567028</h3>
+            </div>
+        </td>
+        <td style="text-align:right;" colspan="3">
+            <h4>Kin, le {{$lelo}}</h4>
+            <h4 class="p-2 bg-gray-200" style="width: 100%;">Montant :  <b style="padding: 5px; background-color:rgb(98, 172, 241); width:100%;color:white">{{ $records[0]->montant }} $ </b></h4>
+            <h4>Loyer de : {{$records[0]->mois}}-{{$records[0]->annee}}</h4>
+        </td>
+        
+    </tr>
+</table>
+<div class="text-center b-2 bg-gray-500 mb-2" style="font-size: 2em;padding:10px; color:rgb(46, 131, 211)">RECU LOYER, PAIEMENT ANTICIPATIF N° {{ $records[0]->id }}</div>
+
+<table class="w-full mb-2" style=" color:rgb(46, 131, 211)">
+    <tr style="font-size: 1.1em; " class="text-bold">
+        <td ><b>Recu de :</b> <span style=" text-decoration:underline;"><b>{{ $records[0]->locataire->noms }}</b>, Galerie: <b>{{ $records[0]->locataire->occupation->galerie->nom }}-{{$records[0]->locataire->occupation->galerie->num}}</b></span></td>
+        {{-- <td ></td> --}}
+    </tr>
+    <tr style="margin-top: 50px; padding-top:10px; width:20px; height:20" class=" py-6">
+        <td><b>Somme de (en toutes lettres)</b> <b style="padding: 5px; background-color:gray; width:100%;"></b>  </td>    
+    </tr>
+    
+    {{-- <tr>
+        <td colspan="1">Pour : </td>
+        <td colspan="3" style="padding: 5px; background-color:gray; width:100%">
+            <b ></b> 
+        </td>
+        
+    </tr> --}}
+    <tr style="">
+        <td colspan="" style="">
+            <div style="padding: 12px; background-color: rgb(135, 190, 241); width:95%">
+
+            </div>
+            <div style="padding: 12px; background-color: rgb(135, 190, 241); width:95%">
+
+            </div>
+            <b></b>
+        </td>
+        
+    </tr>
+    <tr style="">
+        <td colspan="" style="padding: 12px; background-color:rgb(135, 190, 241; width:100%">
+            <b></b>
+        </td>
+        
+    </tr>
+    <tr>
+        <td style=" width:100%"><b>Pour :</b> ............................................................................................................................................</td>
+    </tr>
+    <tr>
+        <td style=" width:100%">........................................................................................................................................................</td>
+    </tr>
+    <tr>
+        <td><b>Visa Bailleur</b> </td>
+        <td style=" width:95%; padding-right: 30px"><b>Visa Locataire</b> </td>
+        
+        {{-- <span>Visa Locataire</span> --}}
+    </tr>
+    
+   {{--  <tr class="">
+        <td>Occupation {{ $record->locataire->occupation->ref }} </td>
+    </tr>
+    <tr class="">
+        <td>
+            C/{{ $record->locataire->occupation->galerie->commune->nom }},
+            Av/{{ $record->locataire->occupation->galerie->av }}, 
+            N° {{ $record->locataire->occupation->galerie->num }}
+        </td>
+    </tr> --}}
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- 
+
+
 <div class="screen-full">
 
     <div class=" text-center">
@@ -70,4 +191,4 @@
         <p>Aujourd'hui le, {{$lelo}}</p>
     </div>
 
-</div>    
+</div>     --}}
