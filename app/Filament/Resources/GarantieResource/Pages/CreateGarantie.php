39c5fd691262dbbf0d3d0bc87adf117ff112815d
ChristianNkelani <christian.n.hiram@gmail.com>
2024-04-25 10:29:30 +0100

    gara

--- app/Filament/Resources/GarantieResource/Pages/CreateGarantie.php
+++ app/Filament/Resources/GarantieResource/Pages/CreateGarantie.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\GarantieResource\Pages;
 
 use Filament\Actions;
 use Filament\Actions\Action;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Resources\GarantieResource;
 
@@ -20,5 +21,12 @@ class CreateGarantie extends CreateRecord
     protected function getCancelFormAction(): Action
     {
         return Action::make('Annuler')->url($this->getResource()::getUrl('index'));
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        //$data['user_id'] = auth()->id();
+        $data['users_id'] = Auth::user()->id;
+        return $data;
     }
 }
