6759a9832afa9a1c051c51daa5a87bf5e0d0ed4a
ChristianNkelani <christian.n.hiram@gmail.com>
2024-04-27 01:12:17 +0100

    comre

--- resources/views/partiel.blade.php
+++ resources/views/partiel.blade.php
@@ -48,7 +48,7 @@
                 </td>
     
                 <td class="fi-ta-cell p-0 first-of-type:ps-1 last-of-type:pe-1 sm:first-of-type:ps-3 sm:last-of-type:pe-3">
-                    ResteLocataire
+                    Reste
                 </td>
             </tr>
         </thead>
